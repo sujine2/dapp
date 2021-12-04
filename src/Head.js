9c87cb5140ce9470e9ade9274665d3e0f01a0cf1
sujine2 <twinsujin22@gmail.com>
2021-12-05 01:17:08 +0900

    first

---
+++ src/Head.js
@@ -0,0 +1,16 @@
+import './Head.css';
+import { Button, Row, Col, Container } from 'react-bootstrap';
+
+function Head() {
+  return (
+    <div className="head">
+      <Container fluid="md">
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Head;
